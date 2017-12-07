b815e61c1de51c11d6a5ac16b6344273d14a7228
彭宣童 <994684265@qq.com>
2017-12-07 15:54:34 +0800

    Add files via upload

---
+++ fusion.h
@@ -0,0 +1,2 @@
+#include<iostream>
+void Re_fusion(const char * BufferIn0,const char * BufferIn1,const char * BufferIn2,const char * BufferOut,int win_size,int classnum,float L_err,float M_err,int A,int pf,int pc);
